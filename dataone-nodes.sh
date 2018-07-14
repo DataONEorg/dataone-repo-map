751196fdded4f5780a1b9fc06543095a6789be70
Matt Jones <gitcode@magisa.org>
2018-07-13 18:41:53 -0800

    Added shell script for updating the node data file.

---
+++ dataone-nodes.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "year,label,description,lon,lat,alt,name,uri" > dataone-nodes.csv
+curl -s "https://cn.dataone.org/cn/v2/node" | xml sel -T -t \
+ -m "//node[(property/@key='CN_operational_status' and property='operational' or property='replicator') and identifier!='urn:node:CN']" \
+ -v "substring(property[@key='CN_date_operational'],1,4)" -o ",\"" \
+ -v "property[@key='CN_node_name']" -o "\",\"" \
+ -v "normalize-space(description)" -o "\"," \
+ -v "property[@key='CN_location_lonlat']" -o ",0," \
+ -v identifier -o "," \
+ -v "property[@key='CN_logo_url']" -n \
+>> dataone-nodes.csv
